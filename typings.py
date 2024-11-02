bf1f32437b4eed25adb2a2a07bcac4cac1ae7fe8
Vladislav Yaremenko <vlad.yaremenko.98@yandex.ru>
2024-11-02 08:04:20 +0300

    Add prefix 'type' in typings.

--- typings.py
+++ typings.py
@@ -1,3 +1,3 @@
-RubricsData = dict[str, dict[str, str | list[dict[str, str]]]]
-FirmRubricData = dict[str, list[dict[str, list[dict[str, str]]]]]
-FirmSubrubricData = dict[str, list[dict[str, str]]]
+type RubricsData = dict[str, dict[str, str | list[dict[str, str]]]]
+type FirmRubricData = dict[str, list[dict[str, list[dict[str, str]]]]]
+type FirmSubrubricData = dict[str, list[dict[str, str]]]
